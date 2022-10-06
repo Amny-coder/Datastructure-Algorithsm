343e9b9eb37d863b258ce06ad3f8be8836e4232b
Amny-coder <usmanmuhammadabdulkadir@gmail.com>
2022-10-06 21:43:38 +0100

    testing changes made in DoublyLinkedList class

--- Main.java
+++ Main.java
@@ -5,16 +5,20 @@ public class Main {
         list.add(0);
         list.add(1);
         list.add(2);
+        list.add(2);
 
         list.addToStart(3);
-        list.addToStart(4);
+        //list.addToStart(4);
 
         list.addAt(0, 45);
-        list.addAt(1, 50);
+        /*list.addAt(1, 50);
 
         list.delete();
-        list.deleteAt(1);
+        list.deleteAt(1);*/
+        System.out.println("Size of the list: " + list.size());
 
+        if (list.isEmpty())
+            System.out.println("Empty list " + list.size());
         list.show();
     }
 }
