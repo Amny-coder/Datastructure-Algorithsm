6065d215c6d492286dcfe2cb0eb409367ee175ae
Amny-coder <usmanmuhammadabdulkadir@gmail.com>
2022-10-08 11:30:27 +0100

    testing the EmplyeeList class

--- genericimplementation/Tester.java
+++ genericimplementation/Tester.java
@@ -2,18 +2,11 @@ package genericimplementation;
 
 public class Tester {
     public static void main(String[] args) {
-        SinglyLinkedList<String> name = new SinglyLinkedList<>();
+        EmployeeList<String> list = new EmployeeList<>();
 
-        name.add("Amir");
-        name.add("Mahmud");
-        name.add("Usman");
-        name.add("Baba");
-        name.addAt(3, "Abdul");
+        list.add("Usman", "123Exz");
+        list.addAt(1, "Mahmud", "12_xx&_12");
 
-        name.remove();
-        name.removeAt(0);
-
-        name.show();
-        System.out.println(name.size() + " " + name.isEmpty());
+        list.displayList();
     }
 }
