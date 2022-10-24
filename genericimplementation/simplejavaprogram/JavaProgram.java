62c1f3469f57ad5e30d7aad174743d42d45a5f46
Amny-coder <amny.inc@gmail.com>
2022-10-24 10:30:13 +0100

    variables declaration and initialization

---
+++ genericimplementation/simplejavaprogram/JavaProgram.java
@@ -0,0 +1,17 @@
+package genericimplementation.simplejavaprogram;
+
+public class JavaProgram {
+    /**
+     * Author: Usman Muhammad
+     * Purpose: Siwes report
+     * Course of study: Bsc Computer science
+     * Program Description: Using variables in Java program
+     */
+    public static void main(String[] args) {
+        String myDate; //variable declaration
+        myDate = "10/19/2022"; //variable initialization
+
+        System.out.println("To days date is: " + myDate);
+
+    }
+}
