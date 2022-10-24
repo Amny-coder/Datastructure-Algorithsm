7de57eeb154f74cf8d7d1f429732e89c2b2a2878
Amny-coder <amny.inc@gmail.com>
2022-10-24 10:25:41 +0100

    Demo program fomatting output with DecimalFormat java std lib class

---
+++ genericimplementation/simplejavaprogram/DecFormat.java
@@ -0,0 +1,14 @@
+package genericimplementation.simplejavaprogram;
+
+public class DecFormat {
+    /**
+     * Author: Usman Muhammad
+     * Purpose: Siwes report
+     * Course of study: Bsc Computer science
+     * Program Description: Demonstration of the DecimalFormat class.
+     */
+
+    public static void main(String[] args) {
+
+    }
+}
